6fe5dd7a4fd3c72e2880c6eafea9b7187663962a
xrmaalk <xinra.inc@gmail.com>
2024-05-03 22:30:26 -0600

    setup postgres prisma

---
+++ prisma/migrations/20240504033739_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Post_name_idx" ON "Post"("name");
